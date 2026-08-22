688b9265c320bfd1c4ce1c5530fe9c04280a0e7d
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-22 20:53:19 +0530

    Implement Maximum Subarray Sum algorithm

---
+++ MaximumSubarraySum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		/* Your class should be named Main.
+ 			* Read input as specified in the question.
+ 			* Print output as specified in the question.
+		*/
+
+		// Write your code here
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		//max = scan.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++){
+			arr[i] = scan.nextInt();
+		}
+		System.out.println(find(n,arr));
+	}
+	private static long find(int n, int[] arr){
+		long res = Long.MIN_VALUE;
+		long curr = 0;
+		for(int i = 0 ; i < n ; i++){
+			curr+=arr[i];
+			res = Math.max(res,curr);
+			if(curr<0){
+				curr = 0;
+			}
+		}
+		return res;
+	}
+
+}
